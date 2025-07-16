db1440f670de21a8460843d406c0a76a5bdebddf
Leo Barreiro <a.leo.barreiro@gmail.com>
2025-07-16 13:49:41 -0300

    feat: alt and ctx

--- keyboards/crkbd/keymaps/barreiroleo/keymap.c
+++ keyboards/crkbd/keymaps/barreiroleo/keymap.c
@@ -4,12 +4,14 @@
 
 // Tap Dance definitions;
 enum {
+    TD_ALT_CTX, // Single tap for Left Alt, twice for Context Menu
+    TD_GUI_ALT, // Single tap for "LWin", twice for Left Alt
     TD_SFT_CAP, // Single tap for Shift, twice for Caps Lock
-    TD_LRALT,   // Single tap for Left Right, twice for Right Alt
 };
 tap_dance_action_t tap_dance_actions[] = {
+    [TD_ALT_CTX] = ACTION_TAP_DANCE_DOUBLE(KC_LALT, KC_APP),
+    [TD_GUI_ALT] = ACTION_TAP_DANCE_DOUBLE(KC_LGUI, KC_LALT),
     [TD_SFT_CAP] = ACTION_TAP_DANCE_DOUBLE(KC_LSFT, KC_CAPS),
-    [TD_LRALT]   = ACTION_TAP_DANCE_DOUBLE(KC_LALT, KC_RALT),
 };
 
 // Layers
@@ -23,7 +25,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LCTL,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_ESC,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LGUI,   MO(1),  KC_SPC,     KC_ENT,   MO(2), TD(TD_LRALT)
+                                     TD(TD_GUI_ALT), MO(1),  KC_SPC,     KC_ENT,   MO(2), TD(TD_ALT_CTX)
                                       //`--------------------------'  `--------------------------'
 
   ),
@@ -36,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LCTL,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,                       KC_F11,  KC_F12, KC_HOME,  KC_END, KC_PGDN,  KC_APP,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LALT, _______,  KC_SPC,     KC_ENT,   MO(3), KC_RALT
+                                     TD(TD_GUI_ALT),_______, KC_SPC,     KC_ENT,   MO(3), TD(TD_ALT_CTX)
                                       //`--------------------------'  `--------------------------'
   ),
 
@@ -48,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LCTL, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, KC_TILD,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LALT,   MO(3),  KC_SPC,     KC_ENT, _______, KC_RALT
+                                     TD(TD_GUI_ALT), MO(3),  KC_SPC,     KC_ENT, _______, TD(TD_ALT_CTX)
                                       //`--------------------------'  `--------------------------'
   ),
 
